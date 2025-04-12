62c54c8afa44992cbedeb42384541bd312ead9e2
Pratik <zajampratik@gmail.com>
2025-04-12 18:39:14 +0530

    added data

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-
 import express from 'express';
-import 'dotenv/config'
-import routes from './src/routes/route.js'
+import 'dotenv/config';
+import routes from './src/routes/route.js';
 import connectDB from './dbConnection.js';
-import cors from 'cors'
+import cors from 'cors';
 import path from 'path';
 
 const app = express();
-app.use(cors())
 const PORT = process.env.PORT || 3000;
+
+// Define custom CORS options
+const corsOptions = {
+  origin: "*", // Allow these origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Enable this if you're using cookies or authorization headers
+};
+
+// Apply CORS middleware with options
+app.use(cors(corsOptions));
 
 const uploadPath = path.join(process.cwd(), 'src', 'uploads');
 app.use('/uploads', express.static(uploadPath));
@@ -16,8 +25,8 @@ app.use('/uploads', express.static(uploadPath));
 app.use(express.json());
 app.use('/', routes);
 
-
-connectDB()
+// Connect to DB and start server
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
